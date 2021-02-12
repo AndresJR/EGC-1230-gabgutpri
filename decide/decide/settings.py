67e038307d0ff32d60d1f9752d029a211d18509f
gabgutpri@alum.us.es <gabgutpri@alum.us.es>
2021-02-12 11:32:31 +0100

    cambios para travis

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+POSTGRESQL_VERSION = ['9.4','9.6']
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
